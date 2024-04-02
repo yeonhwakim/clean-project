5cab04893152dd11d2c8422c76ef18e30953fb61
debby <debby@spitha.io>
2024-04-02 17:48:04 +0900

    feat: trigger

---
+++ features/trigger.js
@@ -0,0 +1,28 @@
+// Get the list of triggers by userId
+export function getTriggerList () {
+    return []
+}
+
+export function getTrigger () {
+    return {}
+}
+
+export function searchTrigger () {
+    return []
+}
+
+export function writeTrigger () {
+    return {}
+}
+
+export function editTrigger () {
+    return {}
+}
+
+export function discardTrigger () {
+    return {}
+}
+
+export function deleteTrigger () {
+    return {}
+}

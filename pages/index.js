40709de552fe00becb8aeec345cee73f8ae8ffd3
김연화 <gcyhkim2@gccorp.com>
2024-04-02 17:25:34 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,4 @@ import { checklistMainPage } from "./checklistMainPage";
 import { checklistEditPage } from "./checklistEditPage";
 import { checklistTriggerPage } from "./checklistTriggerPage";
 
-export {checklistEditPage, checklistEditPage, checklistTriggerPage}
+export {checklistMainPage, checklistEditPage, checklistTriggerPage}

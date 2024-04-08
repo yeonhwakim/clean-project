255c664039b453bba225c4efca410243ddea360c
yeonhwa <yeonhwa6717@gmail.com>
2024-04-08 22:51:14 +0900

    feat: Add layout and page

---
+++ src/pages/home/ui.tsx
@@ -0,0 +1,10 @@
+import CreateItem from "../../features/checklist/create-item/ui";
+import Layout from "../../widgets/layout/ui";
+
+export default function Home() {
+  return (
+    <Layout>
+      <CreateItem />
+    </Layout>
+  );
+}
---
+++ src/widgets/header/ui.tsx
@@ -0,0 +1,10 @@
+import Button from "../../shared/ui/button";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Cliean</h1>
+      <Button name="sign in" onClickHandler={() => {}} />
+    </div>
+  );
+}

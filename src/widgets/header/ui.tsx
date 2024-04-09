2132e24cf2c8d9aa0cd2f4ffdf14cd6e89be8e0c
김연화 <gcyhkim2@gccorp.com>
2024-04-09 15:45:14 +0900

    chore: Render create-item

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -3,8 +3,8 @@ import Button from "../../shared/ui/button";
 export default function Header() {
   return (
     <div>
-      <h1>Cliean</h1>
-      <Button name="sign in" onClickHandler={() => {}} />
+      <h1>Clean Project</h1>
+      <Button type="button" name="sign in" onClickHandler={() => {}} />
     </div>
   );
 }

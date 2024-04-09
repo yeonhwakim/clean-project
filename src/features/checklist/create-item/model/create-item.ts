ce9a83e932108705472d837bbb009814abf672e3
김연화 <gcyhkim2@gccorp.com>
2024-04-09 15:40:49 +0900

    feat: Add create item business logic
    locastorage 값을 가져와서 추가하고 다시 셋팅

--- src/features/checklist/create-item/model/create-item.ts
+++ src/features/checklist/create-item/model/create-item.ts
@@ -1,7 +1,10 @@
-// user model에서 user 정보 조회
+export type Item = { 
+  task: string;
+};
 
-const createItem = () => {
-  // CREATE checklist(item: ChecklistItem)
+const createItem = (item: Item) => {
+  const items = JSON.parse(localStorage.getItem("items") || "[]")
+  localStorage.setItem("items", JSON.stringify([...items, {...item, userId: 1, id: items.length + 1}])); 
 };
 
 export const events = { createItem };

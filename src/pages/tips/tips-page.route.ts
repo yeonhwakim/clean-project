35d5558ccb6d39247dd6c70ab783ba0d4c7509c1
debby <debby@spitha.io>
2024-06-17 13:23:26 +0900

    fix: tips router slug

--- src/pages/tips/tips-page.route.ts
+++ src/pages/tips/tips-page.route.ts
@@ -1,19 +1,20 @@
 import { createElement } from 'react';
 import { RouteObject, redirect } from 'react-router-dom';
 import { TipsPage } from './tips-page.ui';
+import { pathKeys } from '@/shared/lib/react-router';
 
 export const tipsPageRoute: RouteObject = {
   path: 'tips',
   children: [
     {
       index: true,
-      loader: async () => redirect('/tips/1'),
+      loader: async () => redirect(pathKeys.page404()),
     },
     {
-      path: ':tip_id',
+      path: ':slug',
       element: createElement(TipsPage),
       loader: async (args) => {
-        console.log(args.params.tip_id);
+        console.log(args.params.slug);
       }
     }
   ]
--- src/shared/lib/react-router/config.ts
+++ src/shared/lib/react-router/config.ts
@@ -6,4 +6,12 @@ export const pathKeys = {
   page404() {
     return pathKeys.root.concat("404/");
   },
+  tips: {
+    root() {
+      return pathKeys.root.concat("tips/");
+    },
+    bySlug({ slug }: { slug: string }) {
+      return pathKeys.tips.root().concat(slug, '/');
+    },
+  }
 };
